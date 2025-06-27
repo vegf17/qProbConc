9ed31e4286f5e9b9dc73e277d766c28f0aaaa28c
Vitor Fernandes <vegf17@gmail.com>
2025-06-27 19:45:44 +0100

    Added the beginning of the definition of a "fair" scheduler that checks the history path

--- KStep.hs
+++ KStep.hs
@@ -8,8 +8,8 @@ import Control.Monad.Trans.Except
 --import Control.Monad
 --import Control.Monad.Identity
 --import System.Random
---import Data.Either
---import Data.List
+import Data.Either
+import Data.List
 --import Data.Ord
 --import Data.Complex
 --import Data.Ratio
@@ -61,33 +61,63 @@ kStepSch (sch, l@(path, (c, s)), k) =
 
 --START: definition of schedulers without IO--
 --undefined scheduler
-undScheduler :: Sch
-undScheduler _ = Nothing
+undSch :: Sch
+undSch _ = Nothing
 
 --scheduler that chooses always the first element in the list of the possible transitions
-initScheduler :: Sch
-initScheduler (path,(c,s)) =
+initSch :: Sch
+initSch (path,(c,s)) =
   let listDist = runProbT $ runExceptT $ runStateT (small c) s -- [[(Either LMem (C,LMem), Double)]]
   in Just $ [(head listDist, 1)]
 
 --scheduler that chooses always the last element in the list of the possible transitions
-lastScheduler :: Sch
-lastScheduler (path,(c,s)) =
+lastSch :: Sch
+lastSch (path,(c,s)) =
   let listDist = runProbT $ runExceptT $ runStateT (small c) s -- [[(Either LMem (C,LMem), Double)]]
   in Just $ [(last listDist, 1)]
 
 --scheduler that chooses always the middle element in the list of the possible transitions
-middleScheduler :: Sch
-middleScheduler (path,(c,s)) =
+middleSch :: Sch
+middleSch (path,(c,s)) =
   let listDist = runProbT $ runExceptT $ runStateT (small c) s -- [[(Either LMem (C,LMem), Double)]]
       ind = fromIntegral $ floor ((fromIntegral $ length listDist)/2)
   in Just $ [(listDist!!ind, 1)]  
 
 --probabilistic scheduler, which attributes the same probability to all the elements in the list of
 --the possible transitions
-probScheduler :: Sch
-probScheduler (path,(c,s)) =
+probSch :: Sch
+probSch (path,(c,s)) =
   let listDist = runProbT $ runExceptT $ runStateT (small c) s -- [[(Either LMem (C,LMem), Double)]]
       len = fromIntegral $ length listDist
   in Just $ [(dist, 1/len) | dist <- listDist]
+
+
+
+--fair scheduler that checks the history path to see if the current command and the classical state
+--already occurred; if yes, then the scheduler tries another option
+
+--TODO: NEEDS TO CHECK ONLY THE CLASSICAL STATES AND THE COMMAND
+fairSch :: Sch
+fairSch hist@(path,(c,s)) = 
+  let listDist = runProbT $ runExceptT $ runStateT (small c) s -- [[(Either LMem (C,LMem), Double)]]
+      nextDist = notRepeated hist (head listDist, tail listDist)
+  in Just $ [(nextDist, 1)]
+
+notRepeated :: ProbPath -> ([(Either LMem (C,LMem), Double)],[[(Either LMem (C,LMem), Double)]]) -> [(Either LMem (C,LMem), Double)]
+notRepeated _ (next,[]) = next
+notRepeated (path,(c,s)) (next,(h:t)) = if (allLeft || null intersection ) --  || allIn listComProbPath nextR
+                                        then next
+                                        else notRepeated (path,(c,s)) (h,t)
+  where allLeft = and $ map (\x -> isLeft x) (map fst next)
+        nextL = lefts (map fst next)
+        nextR = rights (map fst next)
+        listComProbPath = map fst path
+        intersection = intersect listComProbPath nextR
+
+-- notInProbPath :: Path C -> Next commands -> Bool
+notInProbPath :: [(C,LMem)] -> [(C,LMem)] -> Bool
+notInProbPath _ [] = True
+notInProbPath pathC (cs:t) = if (elem cs pathC)
+                                then False
+                                else notInProbPath pathC t
 --END: definition of schedulers without IO--
